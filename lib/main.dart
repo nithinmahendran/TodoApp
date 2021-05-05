4870d023597513162629341b88fe5a6b57191dc9
Nithin Mahendran <nithinmahendren@gmail.com>
2021-05-05 19:41:25 +0530

    Create Title Top Bar and Button #2

---
+++ lib/main.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'models/global.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Todo App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      color: Colors.yellow,
+      home: SafeArea(
+        child: DefaultTabController(
+          length: 3,
+          child: new Scaffold(
+            body: Stack(children: [
+              TabBarView(
+                children: [
+                  new Container(
+                    color: darkGrey,
+                  ),
+                  new Container(
+                    color: Colors.orange,
+                  ),
+                  new Container(
+                    color: Colors.lightGreen,
+                  ),
+                ],
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 50),
+                height: 160,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(40),
+                        bottomRight: Radius.circular(40)),
+                    color: Colors.white),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Intray",
+                      style: todoTitle,
+                    ),
+                    Container(),
+                  ],
+                ),
+              ),
+              Container(
+                height: 65,
+                width: 65,
+                margin: EdgeInsets.only(
+                    top: 131, left: MediaQuery.of(context).size.width * 0.43),
+                child: FloatingActionButton(
+                  onPressed: () {},
+                  child: Icon(Icons.add),
+                  backgroundColor: addButtonColor,
+                ),
+              )
+            ]),
+            appBar: AppBar(
+              elevation: 0,
+              title: new TabBar(
+                tabs: [
+                  Tab(
+                    icon: new Icon(Icons.home),
+                  ),
+                  Tab(
+                    icon: new Icon(Icons.rss_feed),
+                  ),
+                  Tab(
+                    icon: new Icon(Icons.perm_identity),
+                  ),
+                ],
+                labelColor: darkGrey,
+                unselectedLabelColor: Colors.blue,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicatorPadding: EdgeInsets.all(5.0),
+                indicatorColor: Colors.transparent,
+              ),
+              backgroundColor: Colors.white,
+            ),
+            backgroundColor: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/models/global.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Color darkGrey = new Color(0xff212128);
+
+TextStyle todoTitle = new TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w600,
+    color: darkGrey,
+    fontSize: 64);
+
+Color addButtonColor = new Color(0xff458EFB);
